8e2be734bf56ecc7637e02b7cae640464fd1d740
mike castleman <m@mlcastle.net>
2016-08-06 16:04:22 -0400

    invalidate cloudefront on upload

--- upload.sh
+++ upload.sh
@@ -1,1 +1,6 @@
+#!/bin/sh
+
+set -e
+
 aws --region us-west-1 s3 cp index.html s3://partyparrotops.com/index.html --no-guess-mime-type --content-type 'text/html;charset=utf-8' --content-language 'en-US' --cache-control 'public;max-age=3600'
+aws cloudfront create-invalidation --distribution-id E1CCTPU5KLQ3XK --paths / /index.html
